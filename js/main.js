965dd8ee3f50da73b00974bc727af76fd5ed3106
Bui Quoc Trieu <buiquoctrieu0301@gmail.com>
2020-11-14 18:43:35 +1100

    done carousel (need to fix background overlay)

--- js/main.js
+++ js/main.js
@@ -2,19 +2,13 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: false,
-  autoplay: true,
+  autoplay: false,
   autoplayTimeout: 5000,
   autoplayHoverPause: true,
-  responsiveClass: true,
+  // responsiveClass: true,
   responsive: {
     0: {
       items: 1,
-    },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 5,
     },
   },
 });
